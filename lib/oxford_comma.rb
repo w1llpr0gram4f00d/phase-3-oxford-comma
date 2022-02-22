3be6ea1345fb99464b84f156a11ab93cc6e3fd1e
Marc Cavazos <marcavazos19@icloud.com>
2022-02-22 13:04:25 -0500

    commit tree 321f359b19d1f404248ded3d43915c383415cc1f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+    binding.pry
+    return array.join(" and ") if array.size < 3
+    binding.pry
+    array[-1] = "and #{array[-1]}"
+
+    array.join(", ")
 end
